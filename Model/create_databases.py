e6c4a48765f3fb9b8c24e16eaf8b75a6aa1b310c
Felipe <felypebueno2004@gmail.com>
2025-07-15 22:34:00 -0300

    Criação das tabelas corrigidas

--- Model/create_databases.py
+++ Model/create_databases.py
@@ -10,19 +10,19 @@ try:
     )
     cursor = conn.cursor()
 
-    cursor.execute("""
-        DROP TABLE exportacoes
-    """)
+    # cursor.execute("""
+    #     DROP TABLE exportacoes
+    # """)
 
     cursor.execute("""
-        CREATE TABLE exportacoes(
-            ID SERIAL PRIMARY KEY, 
+        CREATE TABLE IF NOT EXISTS exportacoes(
+            ID_EXPORTACAO SERIAL PRIMARY KEY, 
             CO_ANO INTEGER NOT NULL,
             CO_MES INTEGER NOT NULL,
             CO_NCM INTEGER NOT NULL,
             CO_UNID INTEGER NOT NULL,
             CO_PAIS INTEGER NOT NULL,
-            SG_UF_NCM CHAR NOT NULL,
+            SG_UF_NCM CHAR(2) NOT NULL,
             CO_VIA INTEGER NOT NULL,
             CO_URF INTEGER NOT NULL,
             QT_ESTAT INTEGER NOT NULL,
@@ -30,86 +30,72 @@ try:
             VL_FOB INTEGER NOT NULL      
         )
     """)
+
     cursor.execute("""
-        DROP TABLE NCMs
-    """)
-            
-    cursor.execute("""
-        CREATE TABLE NCMs(
-            ID SERIAL PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS NCMs(
+            ID_NCM INTEGER NOT NULL,
             PRODUTO VARCHAR(100) NOT NULL,
-            CO_NCM INTEGER NOT NULL,
 
-            CONSTRAINT fk_CO_NCM FOREIGN KEY (CO_NCM) REFERENCES exportacoes(CO_NCM)          
+            CONSTRAINT pk_NCMs PRIMARY KEY (ID_NCM),
+                   
+            CONSTRAINT fk_ID_NCM FOREIGN KEY (ID_NCM) REFERENCES exportacoes(CO_NCM)          
         )
     """)
-    
-    cursor.execute("""
-        DROP TABLE Paises
-    """)
 
     cursor.execute("""
-        CREATE TABLE Paises(
-            ID SERIAL PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS Paises(
+            ID_PAIS INTEGER NOT NULL,
             PAIS VARCHAR(100) NOT NULL,
-            CO_PAIS INTEGER NOT NULL,
 
-            CONSTRAINT fk_CO_PAIS FOREIGN KEY (CO_PAIS) REFERENCES exportacoes(CO_PAIS)           
-        )
-    """)
-    
-    cursor.execute("""
-        DROP TABLE URFs
-    """)
-
-    cursor.execute("""
-        CREATE TABLE URFs(
-            ID SERIAL PRIMARY KEY,
-            URF VARCHAR(100) NOT NULL,
-            CO_URF INTEGER NOT NULL, 
-
-            CONSTRAINT fk_CO_URF FOREIGN KEY (CO_URF) REFERENCES exportacoes(CO_URF)          
+            CONSTRAINT pk_Paises PRIMARY KEY (ID_PAIS),
+                   
+            CONSTRAINT fk_ID_PAIS FOREIGN KEY (ID_PAIS) REFERENCES exportacoes(CO_PAIS)           
         )
     """)
 
     cursor.execute("""
-        DROP TABLE Paises_Blocos
+        CREATE TABLE IF NOT EXISTS URFs(
+            ID_URF INTEGER NOT NULL,
+            URF VARCHAR(100) NOT NULL, 
+
+            CONSTRAINT pk_URFs PRIMARY KEY (ID_URF),
+                   
+            CONSTRAINT fk_ID_URF FOREIGN KEY (ID_URF) REFERENCES exportacoes(CO_URF)          
+        )
     """)
 
     cursor.execute("""
-        CREATE TABLE Paises_Blocos(
+        CREATE TABLE IF NOT EXISTS Paises_Blocos(
             ID_PAIS INTEGER NOT NULL,
             ID_BLOCO INTEGER NOT NULL,
                    
             CONSTRAINT pk_Paises_Blocos PRIMARY KEY (ID_PAIS, ID_BLOCO),
-            CONSTRAINT fk_ID_PAIS FOREIGN KEY (ID_PAIS) REFERENCES Paises(ID),
-            CONSTRAINT fk_ID_BLOCO FOREIGN KEY (ID_BLOCO) REFERENCES Blocos_Economicos(ID)
+                   
+            CONSTRAINT fk_ID_PAIS FOREIGN KEY (ID_PAIS) REFERENCES Paises(ID_PAIS),
+                   
+            CONSTRAINT fk_ID_BLOCO FOREIGN KEY (ID_BLOCO) REFERENCES Blocos_Economicos(ID_BLOCO)
         )
     """)
 
     cursor.execute("""
-        DROP TABLE Vias
-    """)
-
-    cursor.execute("""
-        CREATE TABLE Vias(
+        CREATE TABLE IF NOT EXISTS Vias(
             ID_VIA INTEGER NOT NULL,
-            VIA VARCHAR(100) NOT NULL,           
+            VIA VARCHAR(100) NOT NULL, 
+
+            CONSTRAINT pk_Vias PRIMARY KEY (ID_VIA),          
             
-            CONSTRAINT fk_CO_VIA FOREIGN KEY (ID_VIA) REFERENCES exportacoes(CO_VIA)
+            CONSTRAINT fk_ID_VIA FOREIGN KEY (ID_VIA) REFERENCES exportacoes(CO_VIA)
         )
     """)
 
     cursor.execute("""
-        DROP TABLE Blocos_Economicos
-    """)
-
-    cursor.execute("""
-        CREATE TABLE Blocos_Economicos(
+        CREATE TABLE IF NOT EXISTS Blocos_Economicos(
             ID_BLOCO INTEGER NOT NULL,
             BLOCO VARCHAR(100) NOT NULL,
+                   
+            CONSTRAINT pk_Blocos_Economicos PRIMARY KEY (ID_BLOCO),
 
-            CONSTRAINT fk_BLOCO FOREIGN KEY (ID_BLOCO) REFERENCES Paises_Blocos(ID_PAIS)           
+            CONSTRAINT fk_ID_BLOCO FOREIGN KEY (ID_BLOCO) REFERENCES Paises_Blocos(ID_BLOCO)           
         )
     """)
 
