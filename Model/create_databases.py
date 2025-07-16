5cf385be86039a0212ce37bbcb2d04f79a95512e
Felipe <felypebueno2004@gmail.com>
2025-07-16 10:57:05 -0300

    Mudança da ordem de criação das tabelas

--- Model/create_databases.py
+++ Model/create_databases.py
@@ -10,60 +10,70 @@ try:
     )
     cursor = conn.cursor()
 
-    # cursor.execute("""
-    #     DROP TABLE exportacoes
-    # """)
-
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS exportacoes(
-            ID_EXPORTACAO SERIAL PRIMARY KEY, 
-            CO_ANO INTEGER NOT NULL,
-            CO_MES INTEGER NOT NULL,
-            CO_NCM INTEGER NOT NULL,
-            CO_UNID INTEGER NOT NULL,
-            CO_PAIS INTEGER NOT NULL,
-            SG_UF_NCM CHAR(2) NOT NULL,
-            CO_VIA INTEGER NOT NULL,
-            CO_URF INTEGER NOT NULL,
-            QT_ESTAT INTEGER NOT NULL,
-            KG_LIQUIDO INTEGER NOT NULL,
-            VL_FOB INTEGER NOT NULL      
-        )
+        DROP TABLE NCMs
     """)
-
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS NCMs(
             ID_NCM INTEGER NOT NULL,
             PRODUTO VARCHAR(100) NOT NULL,
 
-            CONSTRAINT pk_NCMs PRIMARY KEY (ID_NCM),
-                   
-            CONSTRAINT fk_ID_NCM FOREIGN KEY (ID_NCM) REFERENCES exportacoes(CO_NCM)          
+            CONSTRAINT pk_NCMs PRIMARY KEY (ID_NCM)          
         )
     """)
 
+    cursor.execute("""
+        DROP TABLE Paises
+    """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS Paises(
             ID_PAIS INTEGER NOT NULL,
             PAIS VARCHAR(100) NOT NULL,
 
-            CONSTRAINT pk_Paises PRIMARY KEY (ID_PAIS),
-                   
-            CONSTRAINT fk_ID_PAIS FOREIGN KEY (ID_PAIS) REFERENCES exportacoes(CO_PAIS)           
+            CONSTRAINT pk_Paises PRIMARY KEY (ID_PAIS)           
         )
     """)
 
+    cursor.execute("""
+        DROP TABLE URFs
+    """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS URFs(
             ID_URF INTEGER NOT NULL,
             URF VARCHAR(100) NOT NULL, 
 
-            CONSTRAINT pk_URFs PRIMARY KEY (ID_URF),
-                   
-            CONSTRAINT fk_ID_URF FOREIGN KEY (ID_URF) REFERENCES exportacoes(CO_URF)          
+            CONSTRAINT pk_URFs PRIMARY KEY (ID_URF)          
         )
     """)
 
+    cursor.execute("""
+        DROP TABLE Vias
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Vias(
+            ID_VIA INTEGER NOT NULL,
+            VIA VARCHAR(100) NOT NULL, 
+
+            CONSTRAINT pk_Vias PRIMARY KEY (ID_VIA)
+        )
+    """)
+
+    cursor.execute("""
+        DROP TABLE Blocos_Economicos
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Blocos_Economicos(
+            ID_BLOCO INTEGER NOT NULL,
+            BLOCO VARCHAR(100) NOT NULL,
+                   
+            CONSTRAINT pk_Blocos_Economicos PRIMARY KEY (ID_BLOCO)           
+        )
+    """)
+
+        
+    cursor.execute("""
+        DROP TABLE Paises_Blocos
+    """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS Paises_Blocos(
             ID_PAIS INTEGER NOT NULL,
@@ -78,25 +88,28 @@ try:
     """)
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS Vias(
-            ID_VIA INTEGER NOT NULL,
-            VIA VARCHAR(100) NOT NULL, 
-
-            CONSTRAINT pk_Vias PRIMARY KEY (ID_VIA),          
-            
-            CONSTRAINT fk_ID_VIA FOREIGN KEY (ID_VIA) REFERENCES exportacoes(CO_VIA)
-        )
+        DROP TABLE exportacoes
     """)
-
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS Blocos_Economicos(
-            ID_BLOCO INTEGER NOT NULL,
-            BLOCO VARCHAR(100) NOT NULL,
-                   
-            CONSTRAINT pk_Blocos_Economicos PRIMARY KEY (ID_BLOCO),
+        CREATE TABLE IF NOT EXISTS exportacoes(
+            ID_EXPORTACAO SERIAL PRIMARY KEY, 
+            CO_ANO INTEGER NOT NULL,
+            CO_MES INTEGER NOT NULL,
+            CO_NCM INTEGER NOT NULL,
+            CO_UNID INTEGER NOT NULL,
+            CO_PAIS INTEGER NOT NULL,
+            SG_UF_NCM CHAR(2) NOT NULL,
+            CO_VIA INTEGER NOT NULL,
+            CO_URF INTEGER NOT NULL,
+            QT_ESTAT INTEGER NOT NULL,
+            KG_LIQUIDO INTEGER NOT NULL,
+            VL_FOB INTEGER NOT NULL,
 
-            CONSTRAINT fk_ID_BLOCO FOREIGN KEY (ID_BLOCO) REFERENCES Paises_Blocos(ID_BLOCO)           
-        )
+            CONSTRAINT fk_NCM FOREIGN KEY (CO_NCM) REFERENCES NCMs(ID_NCM),      
+            CONSTRAINT fk_PAIS FOREIGN KEY (CO_PAIS) REFERENCES Paises(ID_PAIS),      
+            CONSTRAINT fk_URF FOREIGN KEY (CO_URF) REFERENCES URFs(ID_URF),      
+            CONSTRAINT fk_VIA FOREIGN KEY (CO_VIA) REFERENCES Vias(ID_VIA)      
+        )  
     """)
 
     conn.commit()
